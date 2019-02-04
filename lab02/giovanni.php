ac6e772befeb971863435ebc13e9a682fb7a0c25
definitelynotavirus <jdalton9@u.rochester.edu>
2019-02-04 11:56:52 -0500

    Latest

--- lab02/giovanni.php
+++ lab02/giovanni.php
@@ -1,86 +1,87 @@
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <?php include 'includes/head.php';?>
+    <head>
+        <?php include 'includes/head.php';?>
 
-    <title>Student | Giovanni Ilacqua | Lab 1: Initial Personal Website</title>
+        <title>Student | Giovanni Ilacqua | Lab 1: Initial Personal Website</title>
 
-	
-</head>
+    	
+    </head>
 
-<body class="container">
+    <body class="container">
 
-    <header>
-        <a href="index.php">
-            <h1>Giovanni Ilacqua</h1>
-            <img class="profile-picture" src="images/profile_photo.jpg" alt="A profile photo">
-        </a>
-    </header>
+        <header>
+            <a href="index.php">
+                <h1>Giovanni Ilacqua</h1>
+                <img class="profile-picture" src="images/profile_photo.jpg" alt="A profile photo">
+            </a>
+        </header>
 
-    <aside>
-        <ul>
-            <li>Computer Science Major</li>
-            <li>Qualified Fitness Instructor</li>
-            <li>Graphic Designer</li>
-        </ul>
-    </aside>    
-
-    <article>
-        <p>I was born in Sicily in the city of Messina but I moved to Milan when I was 3 years old. After studying Information
-            Technology and Science at high school, I decided I wanted to attend University abroad. Originally I intended to
-            study in the US but I then decided to apply to universities in the UK, because studying in America was extremely
-            expensive. At 19 I therefore moved to England to study Computer Science at the University of York.
-            I studied 2 years at York and although the campus and the courses were great I still had the dream to study in the
-            US!
-            So I applied to the exchange program for North America and I managed to be selected for the University of
-            Rochester.
-        </p>
-    </article>
-
-    <article>
-        <h2>Extra Curriculars</h2>
-        <p>I am currently spending my year abroad at Rochester and I have to say that so far it has been one of the greatest
-            experiences of my life. Since the moment I arrived here I felt like it was the right place for me, from the people
-            I met to the very stimulating environment that this campus offers.
-            Besides focusing on my academics I also decided to get involved in extra curricular activities, so I ended up
-            creating a fitness club called Meliora Fitness to promote the benefits of physical exercise and provide free
-            bodyweight training classes to students.
-            Additionally I also took part in Greek Life by joining Beta Theta Pi, a fraternity in which I was able to find many
-            smart and like minded people.
-        </p>
-        
-    </article>
+        <aside>
+            <ul>
+                <li>Computer Science Major</li>
+                <li>Qualified Fitness Instructor</li>
+                <li>Graphic Designer</li>
+            </ul>
+        </aside>    
 
         <article>
-        <h2>Courses</h2>
-        <p>In my Fall semester I took 5 classes: CAS 170 which is a class for international students regarding the American
-            culture, CSC 240 Data Mining which was quite interesting but I realized that it is not an area I want to expand on,
-            CSC 214 iOS Development where I learned and experimented with Swift, CSC 170 Intro to Web Development which allowed
-            to learn web development in a formal way and also made me practice my design skills, finally CSC 210 which focused
-            more on Back End technologies and was a great complement to CSC 170.
-        </p>
+            <p>I was born in Sicily in the city of Messina but I moved to Milan when I was 3 years old. After studying Information
+                Technology and Science at high school, I decided I wanted to attend University abroad. Originally I intended to
+                study in the US but I then decided to apply to universities in the UK, because studying in America was extremely
+                expensive. At 19 I therefore moved to England to study Computer Science at the University of York.
+                I studied 2 years at York and although the campus and the courses were great I still had the dream to study in the
+                US!
+                So I applied to the exchange program for North America and I managed to be selected for the University of
+                Rochester.
+            </p>
+        </article>
+
+        <article>
+            <h2>Extra Curriculars</h2>
+            <p>I am currently spending my year abroad at Rochester and I have to say that so far it has been one of the greatest
+                experiences of my life. Since the moment I arrived here I felt like it was the right place for me, from the people
+                I met to the very stimulating environment that this campus offers.
+                Besides focusing on my academics I also decided to get involved in extra curricular activities, so I ended up
+                creating a fitness club called Meliora Fitness to promote the benefits of physical exercise and provide free
+                bodyweight training classes to students.
+                Additionally I also took part in Greek Life by joining Beta Theta Pi, a fraternity in which I was able to find many
+                smart and like minded people.
+            </p>
+            
+        </article>
+
+            <article>
+            <h2>Courses</h2>
+            <p>In my Fall semester I took 5 classes: CAS 170 which is a class for international students regarding the American
+                culture, CSC 240 Data Mining which was quite interesting but I realized that it is not an area I want to expand on,
+                CSC 214 iOS Development where I learned and experimented with Swift, CSC 170 Intro to Web Development which allowed
+                to learn web development in a formal way and also made me practice my design skills, finally CSC 210 which focused
+                more on Back End technologies and was a great complement to CSC 170.
+            </p>
+            
+        <article>
+
+       	<article>        
+            <h2>Future Plans</h2>
+            <p>I am enjoying my experience at Rochester so much that I am considering transferring and finishing my degree there.
+                Although the transfer process is quite complex for my situation, since I am a third year student, I believe it will
+                be worth graduating in the US. Especially considering my goal to eventually move to California and create a
+                business there.
+            </p>
+            
+        <article>
+
+        <?php include 'includes/nav.php';?>
+
+        <footer>
+            CSC 174: Advanced Front-end Web Design and Development
+        </footer>
+
         
-    <article>
 
-   	<article>        
-        <h2>Future Plans</h2>
-        <p>I am enjoying my experience at Rochester so much that I am considering transferring and finishing my degree there.
-            Although the transfer process is quite complex for my situation, since I am a third year student, I believe it will
-            be worth graduating in the US. Especially considering my goal to eventually move to California and create a
-            business there.
-        </p>
-        
-    <article>
-
-    <?php include 'includes/nav.php';?>
-
-    <footer>
-        CSC 174: Advanced Front-end Web Design and Development
-    </footer>
-
-    
-
-    <?php include 'includes/scripts.php';?>
-</body>
+        <?php include 'includes/scripts.php';?>
+    </body>
 
 
 </html>
