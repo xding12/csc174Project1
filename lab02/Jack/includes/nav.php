d2483e2d15ffb44666d925ac82225217a9729581
definitelynotavirus <jdalton9@u.rochester.edu>
2019-02-04 10:39:43 -0500

    Update Jack's Website

---
+++ lab02/Jack/includes/nav.php
@@ -0,0 +1,19 @@
+<nav>
+	<ul>
+		<li>
+			<a href="./index.php">Home</a>
+		</li>
+		
+		<li>
+			<a href="./early_life.php">Early Life</a>
+		</li>
+		
+		<li>
+			<a href="./work_life.php">Work Life</a>
+		</li>
+		
+		<li>
+			<a href="./free_time.php">Free Time</a>
+		</li>
+	</ul>
+</nav>
