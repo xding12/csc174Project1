b85d6c906f47985dcd3d477acad5854aed0996c4
Genessis <ggalindo@u.rochester.edu>
2019-02-10 14:36:07 -0500

    deleted duplicate nav elements link

--- assignment03/includes/nav.php
+++ assignment03/includes/nav.php
@@ -3,8 +3,6 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="evolution_of_visual_entertainment.php">The Evolution of Visual Entertainment</a></li>
         <li><a href="programming_languages.php">Programming Languages</a></li>
-        <li><a href="evolution_of_visual_entertainment.php">Cats and their Similarities</a></li>
-        <li><a href="programming_languages.php">Programming Languages</a></li>
         <li><a href="cats_and_their_similarities.php">Cats and their Similarities</a></li>
     </ul>
 </nav>
