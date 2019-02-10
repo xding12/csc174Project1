6dd6c4293b2eaa23d703c2a9eb82fd2ea09b804d
definitelynotavirus <jdalton9@u.rochester.edu>
2019-02-10 13:46:29 -0500

    Fix Nav

--- assignment03/includes/nav.php
+++ assignment03/includes/nav.php
@@ -3,6 +3,6 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="evolution_of_visual_entertainment.php">The Evolution of Visual Entertainment</a></li>
         <li><a href="genessis.php">Genessis Galindo</a></li>
-        <li><a href="evolution_of_visual_entertainment.php">Cats and their Similarities</a></li>
+        <li><a href="cats_and_their_similarities.php">Cats and their Similarities</a></li>
     </ul>
 </nav>
